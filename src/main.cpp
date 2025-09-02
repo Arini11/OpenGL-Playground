f5924f8befdadf8d8726bcf5a4355e1317fe1b53
Arnau <elarnaumc@gmail.com>
2025-09-02 18:42:34 +0200

    organizar codigo

--- src/main.cpp
+++ src/main.cpp
@@ -4,77 +4,100 @@
 #include <iostream>
 #include <cmath>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "shaderClass.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+void vertexSetup();
+GLFWwindow* init();
 
-// settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// Vertices coordinates
+/*
+GLfloat vertices[] =
+{
+//    X                         Y
+	-0.5f     , -0.5f * float(sqrt(3)) / 3     , 0.0f, // Lower left corner
+	0.5f      , -0.5f * float(sqrt(3)) / 3     , 0.0f, // Lower right corner
+	0.0f      , 0.5f  * float(sqrt(3)) * 2 / 3 , 0.0f, // Upper corner
+	-0.5f / 2 , 0.5f  * float(sqrt(3)) / 6     , 0.0f, // Inner left
+	0.5f / 2  , 0.5f  * float(sqrt(3)) / 6     , 0.0f, // Inner right
+	0.0f      , -0.5f * float(sqrt(3)) / 3     , 0.0f // Inner down
+};
+
+GLfloat vertices[] =
+{
+//    X                         Y
+	-1.0f     , -1.0f * float(sqrt(3)) / 3     , 0.0f, // Lower left corner
+	1.0f      , -1.0f * float(sqrt(3)) / 3     , 0.0f, // Lower right corner
+	0.0f      , 1.0f  * float(sqrt(3)) / 3 , 0.0f, // Upper corner
+	-1.0f / 2 , 1.0f  * float(sqrt(3)) / 6     , 0.0f, // Inner left
+	1.0f / 2  , 1.0f  * float(sqrt(3)) / 6     , 0.0f, // Inner right
+	0.0f      , -1.0f * float(sqrt(3)) / 3     , 0.0f // Inner down
+};
+
+// Indices for vertices order
+GLuint indices[] =
+{
+	0, 3, 5, // Lower left triangle
+	3, 2, 4, // Lower right triangle
+	5, 4, 1 // Upper triangle
+};
+*/
+
+
+GLfloat vertices[] =
+{
+//    X                         Y
+	-1.0f     , -1.0f     , 0.0f, // Lower left corner
+	1.0f      , -1.0f     , 0.0f, // Lower right corner
+	0.0f      , 1.0f      , 0.0f, // Upper corner
+};
+GLuint indices[] =
+{
+	0, 1, 2
+};
+
+
+float rotation = 0.0f; // ángulo en radianes
+
+
+
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
+    // 1. Inicializar
+    GLFWwindow* window = init();
 
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-
+    // 2. Shaders
     Shader shaderProgram("default.vert", "default.frag");
 
+    // 3. VAO / VBO
+	VAO VAO1;
+	VAO1.Bind();
 
-    // ---------------------------
-    // 2) DATOS Y BUFFERS (VAO/VBO)
-    //    (también antes del bucle)
-    // ---------------------------
-	GLfloat vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
-	};
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO VBO1(vertices, sizeof(vertices));
+	// Generates Element Buffer Object and links it to indices
+	EBO EBO1(indices, sizeof(indices));
 
-    GLuint VAO = 0, VBO = 0;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+	// Links VBO to VAO
+	VAO1.LinkVBO(VBO1, 0);
 
-    glBindVertexArray(VAO);
+	// Unbind all to prevent accidentally modifying them
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Atributo 0: aPos (vec2)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
 
     // render loop
     // -----------
@@ -89,14 +112,13 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // ---------------------------
-        // 3) DIBUJAR EL TRIÁNGULO
-        //    (dentro del bucle)
-        // ---------------------------
         shaderProgram.Activate();
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
+
+        // Bind the VAO so OpenGL knows to use it
+		VAO1.Bind();
+
+		// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -107,9 +129,11 @@ int main()
     // ---------------------------
     // 4) LIMPIEZA (al salir)
     // ---------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    shaderProgram.Delete();
+    // Delete all the objects we've created
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -122,7 +146,13 @@ int main()
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
         glfwSetWindowShouldClose(window, true);
+    }
+    else if (glfwGetKey(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+    {
+        rotation += glm::radians(10.0f); // sumar 10 grados
+    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -132,4 +162,32 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+GLFWwindow* init()
+{
+    // glfw: inicializar, con v.3.3
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    // glfw: ventana
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL Playground", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+    }
+
+    return window;
 }
